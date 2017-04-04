25748ae5aa04f9cc94dc091c75f69e325a4a73be
thekom <thekom.7835@gmail.com>
2017-04-04 17:53:28 +0200

    Update events.cljs

--- src/epitaph/events.cljs
+++ src/epitaph/events.cljs
@@ -1,112 +1,7 @@
 (ns epitaph.events)
 
 (def event-info
-  {;; initial extinction events
-
-   :asteroid
-   {:name :asteroid
-    :set-vars {:state :extinct}
-    :desc ["In $STARDATE, $PLANET collided with a $ADJ $OBJ, resulting in a "
-           "mass extinction event which wiped out all traces of $CIV "
-           "civilization."]
-    :vocab {"$ADJ" ["wandering" "wayward"]
-            "$OBJ" ["asteroid" "comet" "planetoid"]}}
-
-   :volcano
-   {:name :volcano
-    :set-vars {:state :extinct}
-    :desc ["In $STARDATE, a massive volcanic eruption filled the skies of "
-           "$PLANET with ash and blotted out the sun. The ensuing volcanic "
-           "winter threw the planet's delicate ecosystem wildly out of "
-           "balance, bringing about the end of $CIV civilization."]}
-
-   :gamma-ray-burst
-   {:name :gamma-ray-burst
-    :set-vars {:state :extinct}
-    :desc ["In $STARDATE, a gamma-ray burst – caused by the explosion of a "
-           "star roughly $DIST $UNIT from the $SYSTEM system – showered "
-           "$PLANET in high energy electromagnetic radiation. The planet's "
-           "atmospheric ozone layer shielded planetary life from immediate "
-           "harm, but was dramatically depleted in the process. Stripped of "
-           "its protection against ordinary UV radiation, the planet's ecosystem "
-           "gradually collapsed, ushering in the end of $CIV civilization."]
-    :vocab {"$DIST" #(+ 900 (* 100 (rand-int 80)))
-            "$UNIT" ["light-years" "parsecs"]}}
-
-   :food-illness
-   {:name :food-illness
-    :set-vars {:state :extinct}
-    :desc ["In $STARDATE, a food-borne illness began to spread rapidly through "
-           "the $CIV population. Less than 10% of the $CIV survived the plague, "
-           "causing a population bottleneck which eventually brought about the "
-           "total collapse of $CIV civilization."]}
-
-   ;; food-related extinction events
-
-   :overhunting
-   {:name :overhunting
-    :set-vars {:state :extinct}
-    :desc ["Due to the extreme effectiveness of stone tools in hunting $BEAST, "
-           "the $CIV managed to hunt the $BEAST species to extinction. Being "
-           "reliant on the $BEAST for food, the $CIV then suffered a famine "
-           "which brought about the end of $CIV civilization."]}
-
-   :overfishing
-   {:name :overfishing
-    :set-vars {:state :extinct}
-    :desc ["As the $CIV population increased, they began to overfish the waters "
-           "of $PLANET. By $STARDATE, they had driven the $FISH species to "
-           "extinction. The ensuing famine brought about a total collapse of "
-           "$CIV civilization."]}
-
-   :crop-failure
-   {:name :crop-failure
-    :set-vars {:state :extinct}
-    :desc ["In $STARDATE, a combination of $ADJ weather and pestilence caused "
-           "a near-total failure of the $CROP crop. Being overreliant on $CROP "
-           "cultivation for food, the $CIV then suffered a massive famine which "
-           "brought about the end of $CIV civilization."]
-    :vocab {"$ADJ" ["inclement" "poor"]}}
-
-   ;; second-tier tech-related extinction events
-
-   :forest-fire
-   {:name :forest-fire
-    :set-vars {:state :extinct}
-    :desc ["In $STARDATE, a cooking fire started by one of the $CIV jumped to "
-           "the forest, where it quickly blazed out of control. When the fire "
-           "finally burned itself out, the forest had been almost completely "
-           "destroyed, disrupting the ecosystem of $PLANET enough to cause a "
-           "total collapse of $CIV civilization."]}
-
-   :war-over-metal
-   {:name :war-over-metal
-    :set-vars {:state :extinct}
-    :desc ["In $STARDATE, due to the growing importance of metal-forged weapons "
-           "in warfare and the scarcity of metal deposits on $PLANET, a massive "
-           "and bloody conflict erupted over control of these deposits. Over 80% "
-           "of the $CIV population was wiped out in the fighting, a loss from "
-           "which $CIV civilization was ultimately unable to recover."]}
-
-   :city-plague
-   {:name :city-plague
-    :set-vars {:state :extinct}
-    :desc ["In $STARDATE, a virulent new plague spread swiftly through the "
-           "largest and densest centers of $CIV population. Living in such "
-           "close proximity, the city-dwelling $CIV were almost entirely wiped "
-           "out by the disease, a loss from which $CIV civilization was "
-           "ultimately unable to recover."]}
-
-   :sea-plague
-   {:name :sea-plague
-    :set-vars {:state :extinct}
-    :desc ["In $STARDATE, a number of $CIV $PEOPLE returned from across the sea "
-           "bearing symptoms of an unfamiliar illness. Having no immunity to "
-           "the germs that caused the disease, the majority of the $CIV "
-           "population was wiped out by the ensuing plague."]
-    :vocab {"$PEOPLE" ["explorers" "traders"]}}
-
-   ;; early flavor events
+  {;; early flavor events
 
    :pets
    {:name :pets
@@ -225,17 +120,6 @@
 
     ;; late-game events
 
-    :bioterrorism
-    {:name :bioterrorism
-     :set-vars {:state :extinct}
-     :desc ["In $STARDATE, a genetically engineered virus designed as a "
-            "highly lethal weapon of biological warfare was deliberately "
-            "distributed in several major centers of $CIV population by an "
-            "agent or agents of unknown affiliation. $CIV medical science "
-            "proved insufficient to combat the ensuing plague, which wiped "
-            "out all but a few isolated pockets of $CIV population and "
-            "brought an end to the era of $CIV technological civilization."]}
-
     :world-government
     {:name :world-government
      :event-chances {:nuclear-weapons -1
@@ -266,35 +150,3 @@
             "obliterated, resulting in the deaths of some $POP,000 $CIV."]
      :vocab {"$SIZE" ["small" "medium-sized" "large" "major"]
              "$POP" #(+ (rand-int 200) 50)}}
-
-    :nuclear-war
-    {:name :nuclear-war
-     :set-vars {:state :extinct}
-     :desc ["In $STARDATE, an early warning system employed by one of the "
-            "major $CIV superpowers detected an incoming nuclear attack. "
-            "Whether the alert was a false alarm remains unclear, but the "
-            "resulting nuclear counterattack and ensuing full-scale nuclear "
-            "war has plunged $PLANET into a state of nuclear winter from "
-            "which it is unlikely that $CIV civilization will ever recover."]}
-
-    :skynet
-    {:name :skynet
-     :prereqs #{:artificial-intelligence}
-     :set-vars {:state :extinct}
-     :desc ["In $STARDATE, an artificially intelligent agent with command "
-            "authority over the military forces of a major $CIV nation "
-            "spontaneously turned against its $CIV masters. Within weeks, the "
-            "thoroughly unprepared $CIV were all but exterminated in a "
-            "seemingly endless wave of attacks by brutally efficient military "
-            "machines."]}
-
-    :gray-goo
-    {:name :gray-goo
-     :set-vars {:state :extinct}
-     :desc ["In $STARDATE, a swarm of self-replicating $CIV nanobots began to "
-            "replicate uncontrollably, devouring vast swaths of $PLANET at a "
-            "rate which $CIV scientists had formerly deemed impossible. After "
-            "several days of rapid expansion, the swarm seems to have become "
-            "dormant, but not before consuming approximately 5% of the entire "
-            "mass of $PLANET and rendering $CIV civilization completely "
-            "extinct."]}})
